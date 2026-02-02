b0d60f686edf8a5b244793f8cb13e9f2b377d126
Huyentsuk0176 <Phthihuyenta03@gmail.com>
2026-02-02 10:07:53 +0900

    session14ex03

--- src/db.sql
+++ src/db.sql
@@ -19,3 +19,18 @@ VALUES (in_name, in_age);
 END //
 
 DELIMITER ;
+delimiter $$
+create procedure update_student(
+    in in_id int,
+    in in_name varchar(100),
+    in in_age int
+)
+begin
+update students
+set name = in_name,
+    age = in_age
+where id = in_id;
+end $$
+delimiter ;
+
+
